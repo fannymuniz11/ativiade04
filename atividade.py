574731deaa671f7cf99c977ae4b8b23cd71c170b
fannymuniz11 <munizfanny03@gmail.com>
2024-09-09 08:49:18 -0300

    atividade04

---
+++ atividade.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+
+N1 = int(input("sua 1 nota"))
+N2 = int(input("sua 2 nota"))
+N3 = int(input("sua 3 nota"))
+
+med = ( N1 + N2 + N3)/3
+
+print(F"Sua nota é {med}")
